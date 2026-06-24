09a57d2525f4ecbf5800c6339b463c0cafa6e46b
Abhishek K <abhishekk@Abhisheks-MacBook-Air.local>
2026-06-24 09:48:41 +0530

    Prepare backend for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,6 +4,12 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/inventory")
 
-engine = create_engine(DATABASE_URL)
+#engine = create_engine(DATABASE_URL)
+#engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
